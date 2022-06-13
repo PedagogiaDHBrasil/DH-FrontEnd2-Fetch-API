4bdf11a56c3d1437fe1bdf4b3d4212e76a6d52d3
andrefpires <andrefpires1@gmail.com>
2022-06-12 23:22:34 -0300

    Tarefa 1

--- js/index.js
+++ js/index.js
@@ -1,20 +1,28 @@
-// Aqui realizamos a consulta da promisse, esperando sua resposta assíncrona
-fetch('https://randomuser.me/api/')
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        //manipulamos a resposta
-        console.log(data)
-    });
+    // Aqui realizamos a consulta da promisse, esperando sua resposta assíncrona
+const urlAPI = 'https://randomuser.me/api/';
 
-function renderizarDadosUsuario(dados) {
+const fetchAPI = (url) => {
     /* -------------------------------- Tarefa 1 -------------------------------- */
     // Aqui devem desenvolver uma função que seja exibida na tela:
     // a foto, o nome completo do usuário e o e-mail.
     // Isto deve ser baseado nas informações que obtemos da API e inseridas no HTML.
-}
+    fetch(url).then(response => response.json().then(data => {
+        const card = document.querySelector('.card');
+        const name = document.createElement('h3');
+        const email = document.createElement('p');
+        const picture = document.createElement('img');
 
+        name.innerHTML = `${data.results[0].name.first}` + ' ' + `${data.results[0].name.last}`;
+        picture.src = `${data.results[0].picture.medium}`;
+        email.innerHTML = `${data.results[0].email}`;
+            
+        card.appendChild(picture);
+        card.appendChild(name);
+        card.appendChild(email);
+    }));
+};
+
+fetchAPI(urlAPI);
 
 /* --------------------------- Tarefa 2 (extra) --------------------------- */
 // Aqui você pode ir para o ponto extra de usar o botão que está comentado no HTML.
